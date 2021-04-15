22e4a0abf108ecfa80a5729ebfd1ca57922ad6db
Neel Bhave <neelbhave91@gmail.com>
2021-04-15 17:42:55 -0400

    Added new Dockerfile for clion connection

---
+++ example/arrow_example.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Neel Bhave on 4/15/21.
+//
+
+#include "arrow_example.h"
+#include "s3select.h"
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+     std::cout<<"hELLO";
+}
