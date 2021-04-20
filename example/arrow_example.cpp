7ba25719879534d70a37034c2686a1f9ec5c3852
Neel Bhave <neelbhave91@gmail.com>
2021-04-20 14:39:27 -0400

    Arrow file generation test

--- example/arrow_example.cpp
+++ example/arrow_example.cpp
@@ -1,7 +1,6 @@
 //
 // Created by Neel Bhave on 4/15/21.
 //
-
 #include "arrow_example.h"
 #include "s3select.h"
 #include <arrow/api.h>
@@ -12,17 +11,52 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-using arrow::Int64Builder;
+#include <arrow/result.h>
+#include <arrow/status.h>
+#include <arrow/table.h>
+#include <arrow/ipc/api.h>
 
+using arrow::Int64Builder;
+using arrow::StringBuilder;
+using arrow::schema;
+using arrow::Table;
+using arrow::Status;
+using arrow::ipc::RecordBatchWriter;
 
 int main(int argc, char** argv)
 {
+    const char* arrow_filename = "test.arrow";
+
     arrow::Int64Builder i64builder;
     i64builder.Append(1);
     i64builder.Append(2);
     i64builder.Append(3);
     std::shared_ptr<arrow::Array> i64array;
     i64builder.Finish(&i64array);
+//    std::cout<<*(i64array.get());
 
-    std::cout<<*(i64array.get());
+    arrow::StringBuilder strbuilder;
+    strbuilder.Append("one");
+    strbuilder.Append("two");
+    strbuilder.Append("three");
+    strbuilder.Append("four");
+    std::shared_ptr<arrow::Array> strarray;
+    strbuilder.Finish(&strarray);
+//    std::cout<<*(strarray.get());
+
+    auto schema = arrow::schema(
+            {arrow::field("int", arrow::int64()),
+             arrow::field("str", arrow::utf8())});
+
+    auto data_table = arrow::Table::Make(schema, {i64array, strarray});
+
+    std::cout<<(data_table.get())->ToString();
+
+    auto output_file = arrow::io::FileOutputStream::Open(arrow_filename);
+
+    auto batch_writer= arrow::ipc::MakeFileWriter(output_file,data_table->schema());
+
+    batch_writer->WriteTable(*data_table);
+
+    batch_writer->Close();
 }
