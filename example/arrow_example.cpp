13f1407851bfc858cc21e75efda1bbb95565ef71
Neel Bhave <neelbhave91@gmail.com>
2021-04-19 20:47:41 -0400

    Basic arrow array creation in example

--- example/arrow_example.cpp
+++ example/arrow_example.cpp
@@ -4,12 +4,25 @@
 
 #include "arrow_example.h"
 #include "s3select.h"
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+//#include <arrow/array/array_base.h>
+//#include <arrow/array/builder_primitive.h>
 #include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+using arrow::Int64Builder;
+
 
 int main(int argc, char** argv)
 {
-     std::cout<<"hELLO";
+    arrow::Int64Builder i64builder;
+    i64builder.Append(1);
+    i64builder.Append(2);
+    i64builder.Append(3);
+    std::shared_ptr<arrow::Array> i64array;
+    i64builder.Finish(&i64array);
+
+    std::cout<<*(i64array.get());
 }
